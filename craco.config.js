8ee9174a54263f0d1f41bda8acb1883639fb3354
vpogodaev <val.pogodaev@gmail.com>
2022-08-21 14:31:45 +0300

    feat: inited

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+const resolvePath = (p) => path.resolve(__dirname, p);
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@app': resolvePath('./src/app'),
+      '@entities': resolvePath('./src/entities'),
+      '@features': resolvePath('./src/features'),
+      '@pages': resolvePath('./src/pages'),
+      '@shared': resolvePath('./src/shared'),
+      '@widgets': resolvePath('./src/widgets'),
+    },
+  },
+};
