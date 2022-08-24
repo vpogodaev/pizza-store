f504580e72fe35d651d0917c11868d9327ced25a
vpogodaev <val.pogodaev@gmail.com>
2022-08-24 22:37:08 +0300

    feat: added back and docker

---
+++ src/frontend/src/app/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Pages } from '@pages/Pages';
+
+function App() {
+  return (
+      <Pages />
+  );
+}
+
+export default App;
---
+++ src/frontend/src/entities/cart/model/cart.ts
@@ -0,0 +1,95 @@
+import { Pizza } from '@entities/pizza/model/pizza';
+import { Misc, MiscType } from '@entities/product/model/misc';
+import { createEvent, createStore, merge, sample } from 'effector';
+
+type PizzasCount = Record<string, number>;
+type MiscCount = Record<MiscType, number>;
+
+// Pizza
+export const addedPizza = createEvent<Pizza>();
+export const removedPizza = createEvent<Pizza>();
+
+export const $pizzasProduct = createStore<Pizza[]>([])
+  .on(addedPizza, (state, pizza) => [...state, pizza])
+  .on(removedPizza, (state, pizza) => {
+    const index = state.findIndex((p) => p.name === pizza.name);
+    return [...state.slice(0, index), ...state.slice(index + 1)];
+  });
+
+export const $pizzasProductsCount = $pizzasProduct.map<PizzasCount>(
+  (pizzas) => {
+    const pizzasCount: PizzasCount = {};
+    pizzas.forEach((p) => {
+      pizzasCount[p.name] = pizzasCount[p.name] + 1 || 1;
+    });
+    return pizzasCount;
+  },
+);
+
+// TODO: remove
+$pizzasProductsCount.watch((state) =>
+  console.log('$pizzasProductsCount.watch', state),
+);
+
+// Misc
+export const addedMisc = createEvent<Misc>();
+export const removedMisc = createEvent<Misc>();
+
+export const $miscListProduct = createStore<Misc[]>([])
+  .on(addedMisc, (state, misc) => [...state, misc])
+  .on(removedMisc, (state, misc) => {
+    const index = state.findIndex((m) => m.type === misc.type);
+    return [...state.slice(0, index), ...state.slice(index + 1)];
+  });
+
+export const $miscProductsCount = $miscListProduct.map<MiscCount>(
+  (miscList) => {
+    const miscCount: MiscCount = {} as MiscCount;
+    miscList.forEach(({ type }) => {
+      miscCount[type] = miscCount[type] + 1 || 1;
+    });
+    return miscCount;
+  },
+);
+
+$miscProductsCount.watch((state) =>
+  console.log('$miscProductsCount.watch', state),
+);
+
+// Price
+const calculatedPrice = createEvent<number>();
+
+const priceChangeEvents = merge([
+  addedPizza,
+  removedPizza,
+  addedMisc,
+  removedMisc,
+]);
+
+export const $totalPrice = createStore<number>(0).on(
+  calculatedPrice,
+  (_, totalPrice) => totalPrice,
+);
+
+sample({
+  clock: priceChangeEvents,
+  source: {
+    pizzas: $pizzasProduct,
+    miscList: $miscListProduct,
+  },
+  fn: ({ pizzas, miscList }) => {
+    const pizzaPrice = pizzas.reduce((acc, pizza) => acc + pizza.price, 0);
+    const miscPrice = miscList.reduce((acc, misc) => acc + misc.price, 0);
+
+    return pizzaPrice + miscPrice;
+  },
+  target: calculatedPrice,
+});
+
+export const pizzaChangedClicked = createEvent<Pizza>();
+
+sample({
+  clock: pizzaChangedClicked,
+  fn: (pizza) => pizza,
+
+})
